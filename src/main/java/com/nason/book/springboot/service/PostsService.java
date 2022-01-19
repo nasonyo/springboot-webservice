5a891c0cd17f2e71e6795189d3ce43cea89ba6b9
nasonyo <ericssoncamel@gmail.com>
2022-01-19 16:26:49 +0900

    Add BaseTimeEntity

---
+++ src/main/java/com/nason/book/springboot/service/PostsService.java
@@ -0,0 +1,39 @@
+package com.nason.book.springboot.service;
+
+import com.nason.book.springboot.domain.posts.Posts;
+import com.nason.book.springboot.domain.posts.PostsRepository;
+import com.nason.book.springboot.web.dto.PostsResponseDto;
+import com.nason.book.springboot.web.dto.PostsSaveRequestDto;
+import com.nason.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
+    }
+}
