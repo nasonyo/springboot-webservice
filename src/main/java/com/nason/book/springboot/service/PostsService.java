6bf7101ef875d55798759dc931f0a188e529e988
nasonyo <ericssoncamel@gmail.com>
2022-03-29 20:14:54 +0900

    Add oauth properfies

--- src/main/java/com/nason/book/springboot/service/PostsService.java
+++ src/main/java/com/nason/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.nason.book.springboot.service;
 
 import com.nason.book.springboot.domain.posts.Posts;
 import com.nason.book.springboot.domain.posts.PostsRepository;
+import com.nason.book.springboot.web.dto.PostsListResponseDto;
 import com.nason.book.springboot.web.dto.PostsResponseDto;
 import com.nason.book.springboot.web.dto.PostsSaveRequestDto;
 import com.nason.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class PostsService {
                 -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
